c0e0d937f8b3cc12cf3c57a72b32313f5f965418
Alex Ballera <alexballera@gmail.com>
2016-07-03 17:13:04 -0400

    Optimizado código de estilos storefront

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,26 @@
 <?php 
 
 /*
+INSERTAR ARCHIVO CSS STOREFRONT, 
+Y ARCHIVOS CSS Y JS COMO THEME CHILD
+
+get_template_directory_uri will look up parent theme location
+get_stylesheet_directory_uri will look up child theme location
+https://ithemes.com/2015/02/17/adding-scripts-wordpress-right-way/
+http://themes.simplethemes.com/skeleton/tutorials/how-to-add-custom-css-and-javascript-using-a-child-theme/
+*/
+function insertScript(){
+        wp_register_script('myScript', get_stylesheet_directory_uri(). '/js/main.min.js', array('jquery'), '1', true );
+        wp_enqueue_script('myScript');
+}
+add_action("wp_enqueue_scripts", "insertScript", 11);
+
+function insertStyle() {
+    wp_enqueue_style( 'myStyle', get_stylesheet_directory_uri().'/css/style.min.css', array('theme'), '1.0', 'screen, projection'); 
+}
+add_action( 'wp_enqueue_scripts', 'insertStyle' );
+
+/*
 AGREGAR MONEDA (Bs.)
 */
 add_filter( 'woocommerce_currencies', 'add_my_currency' );
@@ -15,25 +35,5 @@ function add_my_currency_symbol( $currency_symbol, $currency ) {
      }
      return $currency_symbol;
 }
-
-/*
-INSERTAR ARCHIVO CSS Y JS COMO THEME CHILD
-
-get_template_directory_uri will look up parent theme location
-get_stylesheet_directory_uri will look up child theme location
-https://ithemes.com/2015/02/17/adding-scripts-wordpress-right-way/
-http://themes.simplethemes.com/skeleton/tutorials/how-to-add-custom-css-and-javascript-using-a-child-theme/
-*/
-function insertScript(){
-        wp_register_script('myScript', get_stylesheet_directory_uri(). '/js/main.min.js', array('jquery'), '1', true );
-        wp_enqueue_script('myScript');
-}
-function insertStyle() {
-    wp_enqueue_style( 'myStyle', get_stylesheet_directory_uri().'/css/style.min.css', array('theme'), '1.0', 'screen, projection'); 
-}
-add_action("wp_enqueue_scripts", "insertScript", 11);
-add_action( 'wp_enqueue_scripts', 'insertStyle' );
-
-
 
  ?>
