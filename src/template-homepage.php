0fb402003fffe4c659f6fb410768107b266f900e
Alex Ballera <alexballera@gmail.com>
2016-07-04 17:05:33 -0400

    Agregado template fleslider-principal

--- src/template-homepage.php
+++ src/template-homepage.php
@@ -14,15 +14,7 @@
 
 get_header(); ?>
 
-	<section class="rotate">
-		<div class="flexslider principal">
-			<ul class="slides">
-				<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-mercadopago.jpg"></li>
-				<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-zoom.jpg"></li>
-				<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-hoffman.jpg"></li>
-			</ul>
-		</div>
-	</section>
+	<?php get_template_part( 'templates/flexslider', 'principal' ); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
---
+++ src/templates/flexslider-principal.php
@@ -0,0 +1,9 @@
+<section class="rotate">
+	<div class="flexslider principal">
+		<ul class="slides">
+			<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-mercadopago.jpg"></li>
+			<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-zoom.jpg"></li>
+			<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-hoffman.jpg"></li>
+		</ul>
+	</div>
+</section>
