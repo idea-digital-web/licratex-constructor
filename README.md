9c2dce56fe65bfcb24bf81a0dd6ce2bb7053355a
Alex Ballera <alexballera@gmail.com>
2016-07-04 13:46:35 -0400

    Agregado homepage-template.php

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # wp-theme-child
-Creación de tema hijo en WordPress
-
-[Guía para Storefront Theme Child](http://montalvomiguelo.com/blog/2015/11/22/como-personalizar-storefront-de-woothemes/
-)  
-[Repositorio](https://github.com/stuartduff/storefront-child-theme)
+**Storefront Theme Child** para la creación de una tienda para Idea Digital
--- src/functions.php
+++ src/functions.php
@@ -20,8 +20,17 @@ function insertStyle() {
 }
 add_action( 'wp_enqueue_scripts', 'insertStyle' );
 
+/**
+ * Agregar Google Fonts Asap
+ */
+
+function google_fonts() {
+	wp_enqueue_style( 'asap', '//fonts.googleapis.com/css?family=Asap:400,400italic,700,700italic', array( 'storefront-style' ) );
+}
+add_action( 'wp_enqueue_scripts', 'google_fonts' );
+
 /*
-AGREGAR MONEDA (Bs.)
+Agregar Moneda: Bolívares Débiles (Bs.)
 */
 add_filter( 'woocommerce_currencies', 'add_my_currency' );
 function add_my_currency( $currencies ) {
@@ -36,4 +45,62 @@ function add_my_currency_symbol( $currency_symbol, $currency ) {
      return $currency_symbol;
 }
 
+/**
+ * Customizamos el color color tweaks
+ */
+
+function sf_child_theme_color_cerise_red( $color ) {
+	$color = '#E23D54';
+	return $color;
+}
+add_filter( 'storefront_default_header_background_color', 'sf_child_theme_color_cerise_red' );
+
+function sf_child_theme_color_white( $color ) {
+	$color = '#ffffff';
+	return $color;
+}
+add_filter( 'storefront_default_header_text_color', 'sf_child_theme_color_white' );
+
+function sf_child_theme_color_mine_shaft( $color ) {
+	$color = '#3F3F3F';
+	return $color;
+}
+add_filter( 'storefront_default_button_background_color', 'sf_child_theme_color_mine_shaft' );
+
+function sf_child_theme_color_peter_river( $color ) {
+	$color = '#3498db';
+	return $color;
+}
+add_filter( 'storefront_default_button_alt_background_color', 'sf_child_theme_color_peter_river' );
+add_filter( 'storefront_default_accent_color', 'sf_child_theme_color_peter_river' );
+add_filter( 'storefront_default_footer_link_color', 'sf_child_theme_color_peter_river' );
+
+
+/**
+ * Adjust the storefront homepage template layout
+ */
+
+function sf_child_theme_homepage_layout() {
+	remove_action( 'homepage', 'storefront_homepage_content', 10 );
+	remove_action( 'homepage', 'storefront_featured_products', 40 );
+	remove_action( 'homepage', 'storefront_product_categories', 20 );
+
+	add_action( 'sf_child_theme_before_homepage_content', 'storefront_homepage_content', 10 );
+	add_action( 'sf_child_theme_before_homepage_content', 'storefront_featured_products', 20 );
+}
+add_action( 'init', 'sf_child_theme_homepage_layout' );
+
+/**
+ * Homepage
+ */
+
+function sf_chilld_theme_product_columns( $args ) {
+	$args['limit'] = 12;
+	$args['columns'] = 4;
+	return $args;
+}
+add_filter( 'storefront_featured_products_args', 'sf_chilld_theme_product_columns' );
+add_filter( 'storefront_recent_products_args', 'sf_chilld_theme_product_columns' );
+add_filter( 'storefront_popular_products_args', 'sf_chilld_theme_product_columns' );
+add_filter( 'storefront_on_sale_products_args', 'sf_chilld_theme_product_columns' );
  ?>
