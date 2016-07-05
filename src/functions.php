b3250262588da39beebb223bbd44a9794e8da9e1
Alex Ballera <alexballera@gmail.com>
2016-07-04 21:32:57 -0400

    optimizando mostrar contenidos en las secciones

--- src/functions.php
+++ src/functions.php
@@ -9,16 +9,16 @@ get_stylesheet_directory_uri will look up child theme location
 https://ithemes.com/2015/02/17/adding-scripts-wordpress-right-way/
 http://themes.simplethemes.com/skeleton/tutorials/how-to-add-custom-css-and-javascript-using-a-child-theme/
 */
-function insertScript(){
+function my_scripts(){
         wp_register_script('myScript', get_stylesheet_directory_uri(). '/js/main.min.js', array('jquery'), '1', true );
         wp_enqueue_script('myScript');
 }
-add_action("wp_enqueue_scripts", "insertScript", 11);
+add_action("wp_enqueue_scripts", "my_scripts", 11);
 
-function insertStyle() {
+function my_stylesheet() {
     wp_enqueue_style( 'myStyle', get_stylesheet_directory_uri().'/css/style.min.css', array('theme'), '1.0', 'screen, projection'); 
 }
-add_action( 'wp_enqueue_scripts', 'insertStyle' );
+add_action( 'wp_enqueue_scripts', 'my_stylesheet' );
 
 /**
  * Agregar Google Fonts Asap
@@ -46,40 +46,8 @@ function add_my_currency_symbol( $currency_symbol, $currency ) {
 }
 
 /**
- * Customizamos el color color tweaks
- */
-
-function sf_child_theme_color_cerise_red( $color ) {
-	$color = '#E23D54';
-	return $color;
-}
-add_filter( 'storefront_default_header_background_color', 'sf_child_theme_color_cerise_red' );
-
-function sf_child_theme_color_white( $color ) {
-	$color = '#ffffff';
-	return $color;
-}
-add_filter( 'storefront_default_header_text_color', 'sf_child_theme_color_white' );
-
-function sf_child_theme_color_mine_shaft( $color ) {
-	$color = '#3F3F3F';
-	return $color;
-}
-add_filter( 'storefront_default_button_background_color', 'sf_child_theme_color_mine_shaft' );
-
-function sf_child_theme_color_peter_river( $color ) {
-	$color = '#3498db';
-	return $color;
-}
-add_filter( 'storefront_default_button_alt_background_color', 'sf_child_theme_color_peter_river' );
-add_filter( 'storefront_default_accent_color', 'sf_child_theme_color_peter_river' );
-add_filter( 'storefront_default_footer_link_color', 'sf_child_theme_color_peter_river' );
-
-
-/**
- * Homepage
-
  * CUSTOMIZAMOS TEMPLATE-HOMEPAGE
+
  * Agregamos o eliminamos el hook:
  * @hooked storefront_homepage_content      - 10
  * @hooked storefront_product_categories    - 20
@@ -90,34 +58,20 @@ add_filter( 'storefront_default_footer_link_color', 'sf_child_theme_color_peter_
  * @hooked storefront_best_selling_products - 70
  */
 
-function sf_child_theme_homepage_layout() {
+function custom_homepage_layout() {
 	remove_action( 'homepage', 'storefront_homepage_content', 10 );
 	remove_action( 'homepage', 'storefront_featured_products', 40 );
 	remove_action( 'homepage', 'storefront_product_categories', 20 );
 	remove_action( 'homepage', 'storefront_popular_products', 50 );
-
-	// add_action( 'sf_child_theme_before_homepage_content', 'storefront_homepage_content', 10 );
-	// add_action( 'sf_child_theme_before_homepage_content', 'storefront_featured_products', 20 );
 }
-add_action( 'init', 'sf_child_theme_homepage_layout' );
+add_action( 'init', 'custom_homepage_layout' );
 
-/**
-Customizamos cantidad de productos a mostrar y cantidad
-de columnas
-*/
-
-function sf_chilld_theme_product_columns( $args ) {
-	$args['limit'] = 12;
-	$args['columns'] = 4;
-	return $args;
-}
-add_filter( 'storefront_featured_products_args', 'sf_chilld_theme_product_columns' );
-add_filter( 'storefront_recent_products_args', 'sf_chilld_theme_product_columns' );
-add_filter( 'storefront_popular_products_args', 'sf_chilld_theme_product_columns' );
-add_filter( 'storefront_on_sale_products_args', 'sf_chilld_theme_product_columns' );
 
 /**
  * Customizamos subtítulos a las secciones de Homepage Template
+ limit ~> Cantidad total de productos a mostrar
+ columns ~> Cantidad de columnas de productos a mostrar
+ title ~> Título de la sección
  */
 
 if ( ! function_exists( 'storefront_recent_products' ) ) {
