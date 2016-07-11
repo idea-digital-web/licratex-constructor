eb34fa078680c384418c64e64b5c168b02789805
Alex Ballera <alexballera@gmail.com>
2016-07-11 12:24:47 -0400

    mejorado titulos de las secciones del content

--- src/functions.php
+++ src/functions.php
@@ -321,7 +321,7 @@ function add_my_currency( $currencies ) {
 add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
 function add_my_currency_symbol( $currency_symbol, $currency ) {
      switch( $currency ) {
-          case 'ABC': $currency_symbol = 'Bs.'; break;
+          case 'ABC': $currency_symbol = 'Bs. '; break;
      }
      return $currency_symbol;
 }
