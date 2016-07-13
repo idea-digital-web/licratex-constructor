4636ea2c4906d5bca28afcfe5de2082a821b9ad5
Alex Ballera <alexballera@gmail.com>
2016-07-12 20:40:26 -0400

    Arreglado enlace de footer-credits

--- src/templates/footer-credits.php
+++ src/templates/footer-credits.php
@@ -1,3 +1,3 @@
 <div class="site-footer__credits">
-	<?php bloginfo('blogname');?> J-40281461-5 © 2016 | Desarrollado por <a href="http://www.ideadigital.com.ve" target="_blank">Idea Digital</a>
+	<?php bloginfo('blogname');?> J-40281461-5 © 2016 | Desarrollado por <a href="http://www.ideadigital.com.ve/" target="_blank">Idea Digital</a>
 </div>
