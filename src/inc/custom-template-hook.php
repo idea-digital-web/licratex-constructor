3f792a38b538ed33e8550547ba77a4b08bb62a96
Alex Ballera <alexballera@gmail.com>
2016-07-14 16:24:58 -0400

    Arreglado remover carrito de compras del menu principal

--- src/inc/custom-template-hook.php
+++ src/inc/custom-template-hook.php
@@ -7,6 +7,7 @@ function custom_storefront_template_hook() {
 	remove_action( 'homepage', 'storefront_product_categories', 20 );
 	remove_action( 'homepage', 'storefront_popular_products', 80 );
 	remove_action( 'storefront_footer', 'storefront_credit', 20);
+	remove_action( 'storefront_header', 'storefront_header_cart', 60);
 	add_action( 'storefront_header', 'storefront_secondary_navigation_wrapper', 22 );
 	add_action( 'storefront_header', 'banner_header', 25 );
 	add_action( 'storefront_header', 'header_cart', 35 );
