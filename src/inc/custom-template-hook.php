b26e1542880f3249b0fa8f4b39bb1b7d853c4e31
Alex Ballera <alexballera@gmail.com>
2016-07-15 10:55:23 -0400

    Agrandada imagenes de single

--- src/inc/custom-template-hook.php
+++ src/inc/custom-template-hook.php
@@ -1,13 +1,13 @@
 <?php 
 
 function custom_storefront_template_hook() {
-	remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
 	remove_action( 'homepage', 'storefront_homepage_content', 10 );
 	remove_action( 'homepage', 'storefront_featured_products', 40 );
 	remove_action( 'homepage', 'storefront_product_categories', 20 );
 	remove_action( 'homepage', 'storefront_popular_products', 50 );
-	remove_action( 'storefront_footer', 'storefront_credit', 20);
 	remove_action( 'storefront_header', 'storefront_header_cart', 60);
+	remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+	remove_action( 'storefront_footer', 'storefront_credit', 20);
 	add_action( 'storefront_header', 'storefront_secondary_navigation_wrapper', 22 );
 	add_action( 'storefront_header', 'banner_header', 25 );
 	add_action( 'storefront_header', 'header_cart', 35 );
