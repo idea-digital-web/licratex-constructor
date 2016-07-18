dc55a6b2d859540d1cabcda1eb4c1c043e852312
Alex Ballera <alexballera@gmail.com>
2016-07-18 18:06:22 -0400

    Mejorado estilos de googlemaps

--- src/footer.php
+++ src/footer.php
@@ -10,11 +10,11 @@
 ?>
 
 		</div><!-- .col-full -->
-	</div><!-- #content -->
 		<!-- Agregar Google Maps solo en el hompage -->
 		<?php if (is_front_page() || is_home()): ?> 
 			<?php get_template_part( 'templates/content', 'googlemap' ); ?>
 		<?php endif; ?>
+	</div><!-- #content -->
 
 
 	<?php do_action( 'storefront_before_footer' ); ?>
