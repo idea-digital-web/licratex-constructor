01f3a44a75baa33f816eedf93aa7a978fbed57cc
Alex Ballera <alexballera@gmail.com>
2016-07-21 11:37:57 -0400

    Agregado browserhappy

--- src/header.php
+++ src/header.php
@@ -28,20 +28,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-<!--[if lt  IE  8]>
-    <p  class="browsehappy">
-      Est&#225;s usando  un  navegador <strong>desactualizado</strong>.
-      Por favor <a  href="http://browsehappy.com/">actualiza  tu  navegador</a>
-      Para  mejorar la  experiencia..
-    </p>
-    <![endif]-->
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-1689752734604820",
-        enable_page_level_ads: true
-      });
-    </script>
+
+<?php get_template_part( 'templates/browsehappy' ); ?>
+
 <div id="page" class="hfeed site">
 	<?php
 	do_action( 'storefront_before_header' ); ?>
---
+++ src/templates/browsehappy.php
@@ -0,0 +1,14 @@
+<!--[if lt  IE  8]>
+  <p  class="browsehappy">
+    Est&#225;s usando  un  navegador <strong>desactualizado</strong>.
+    Por favor <a  href="http://browsehappy.com/">actualiza  tu  navegador</a>
+    Para  mejorar la  experiencia..
+  </p>
+<![endif]-->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-1689752734604820",
+    enable_page_level_ads: true
+  });
+</script>
