1cbe2aa6de2f210b7eab0520c3088c09150dd644
Alex Ballera <alexballera@gmail.com>
2016-07-21 10:43:21 -0400

    Customizado functions.php utilizando filters

--- src/inc/custom-content.php
+++ src/inc/custom-content.php
@@ -1,168 +1,45 @@
 <?php 
 /**
  * CONTENT
+ * Establecemos la cantidad de productos y columnas a mostrar
  * Customizamos subtítulos a las secciones de Homepage Template
- * limit ~> Cantidad total de productos a mostrar
- * columns ~> Cantidad de columnas de productos a mostrar
- * title ~> Título de la sección
  */
 
-if ( ! function_exists( 'storefront_recent_products' ) ) {
-	/**
-	 * Display Recent Products
-	 * Hooked into the `homepage` action in the homepage template
-	 *
-	 * @since  1.0.0
-	 * @param array $args the product section args.
-	 * @return void
-	 */
-
-	function storefront_recent_products( $args ) {
-
-		if ( is_woocommerce_activated() ) {
-
-			$args = apply_filters( 'storefront_recent_products_args', array(
-				'limit' 			=> 4,
-				'columns' 			=> 4,
-				'orderby' => 'date',
-				'order'   => 'desc',
-				'title'				=> __( 'Los más recientes', 'storefront' ),
-			) );
-
-			echo '<section class="storefront-product-section storefront-recent-products">';
-
-			do_action( 'storefront_homepage_before_recent_products' );
-
-			echo '<h2 class="section-title">' . wp_kses_post( $args['title'] ) . '</h2>';
-
-			do_action( 'storefront_homepage_after_recent_products_title' );
-
-			echo storefront_do_shortcode( 'recent_products', array(
-				'per_page' => intval( $args['limit'] ),
-				'columns'  => intval( $args['columns'] ),
-			) );
-
-			do_action( 'storefront_homepage_after_recent_products' );
-
-			echo '</section>';
-		}
-	}
+function products_columns( $args ) {
+	$args['limit'] = 4; // limit ~> Cantidad total de productos a mostrar
+	$args['columns'] = 4; // columns ~> Cantidad de columnas de productos a mostrar
+	$args['orderby'] = 'date'; // Ordenamos por fecha
+	$args['order'] = 'desc'; // Ordenamos de manera descendente
+	return $args;
 }
+add_filter( 'storefront_recent_products_args', 'products_columns' );
+add_filter( 'storefront_on_sale_products_args', 'products_columns' );
+add_filter( 'storefront_best_selling_products_args', 'products_columns' );
+add_filter( 'storefront_popular_products_args', 'products_columns' );
+add_filter( 'storefront_featured_products_args', 'products_columns' );
 
-if ( ! function_exists( 'storefront_on_sale_products' ) ) {
-	/**
-	 * Display On Sale Products
-	 * Hooked into the `homepage` action in the homepage template
-	 *
-	 * @param array $args the product section args.
-	 * @since  1.0.0
-	 * @return void
-	 */
+// Editamos los títulos de las secciones
 
-	function storefront_on_sale_products( $args ) {
-
-		if ( is_woocommerce_activated() ) {
-
-			$args = apply_filters( 'storefront_on_sale_products_args', array(
-				'limit'   => 4,
-				'columns' => 4,
-				'orderby' => 'date',
-				'order'   => 'desc',
-				'title'   => __( 'Ofertas', 'storefront' ),
-			) );
-
-			echo '<section class="storefront-product-section storefront-on-sale-products">';
-
-			do_action( 'storefront_homepage_before_on_sale_products' );
-
-			echo '<h2 class="section-title">' . wp_kses_post( $args['title'] ) . '</h2>';
-
-			do_action( 'storefront_homepage_after_on_sale_products_title' );
-
-			echo storefront_do_shortcode( 'sale_products', array(
-				'per_page' => intval( $args['limit'] ),
-				'columns'  => intval( $args['columns'] ),
-			) );
-
-			do_action( 'storefront_homepage_after_on_sale_products' );
-
-			echo '</section>';
-		}
-	}
+function recent_products_title( $args ) {
+	$args['title'] = 'Los más recientes'; // title ~> Título de la sección
+	return $args;
 }
+add_filter( 'storefront_recent_products_args', 'recent_products_title' );
 
-if ( ! function_exists( 'storefront_best_selling_products' ) ) {
-	/**
-	 * Display Best Selling Products
-	 * Hooked into the `homepage` action in the homepage template
-	 *
-	 * @since 2.0.0
-	 * @param array $args the product section args.
-	 * @return void
-	 */
-
-	function storefront_best_selling_products( $args ) {
-		if ( is_woocommerce_activated() ) {
-			$args = apply_filters( 'storefront_best_selling_products_args', array(
-				'limit'   => 4,
-				'columns' => 4,
-				'orderby' => 'date',
-				'order'   => 'desc',
-				'title'	  => esc_attr__( 'Los más vendidos', 'storefront' ),
-			) );
-			echo '<section class="storefront-product-section storefront-best-selling-products">';
-			do_action( 'storefront_homepage_before_best_selling_products' );
-			echo '<h2 class="section-title">' . wp_kses_post( $args['title'] ) . '</h2>';
-
-			do_action( 'storefront_homepage_after_best_selling_products_title' );
-			echo storefront_do_shortcode( 'best_selling_products', array(
-				'per_page' => intval( $args['limit'] ),
-				'columns'  => intval( $args['columns'] ),
-			) );
-			do_action( 'storefront_homepage_after_best_selling_products' );
-			echo '</section>';
-		}
-	}
+function on_sale_products_title( $args ) {
+	$args['title'] = 'Ofertas'; // title ~> Título de la sección
+	return $args;
 }
+add_filter( 'storefront_on_sale_products_args', 'on_sale_products_title' );
 
-if ( ! function_exists( 'storefront_popular_products' ) ) {
-	/**
-	 * Display Popular Products
-	 * Hooked into the `homepage` action in the homepage template
-	 *
-	 * @since  1.0.0
-	 * @param array $args the product section args.
-	 * @return void
-	 */
-
-	function storefront_popular_products( $args ) {
-
-		if ( is_woocommerce_activated() ) {
-
-			$args = apply_filters( 'storefront_popular_products_args', array(
-				'limit'   => 4,
-				'columns' => 4,
-				'orderby' => 'date',
-				'order'   => 'desc',
-				'title'   => __( 'Los más populares', 'storefront' ),
-			) );
-
-			echo '<section class="storefront-product-section storefront-popular-products">';
-
-			do_action( 'storefront_homepage_before_popular_products' );
-
-			echo '<h2 class="section-title">' . wp_kses_post( $args['title'] ) . '</h2>';
-
-			do_action( 'storefront_homepage_after_popular_products_title' );
-
-			echo storefront_do_shortcode( 'top_rated_products', array(
-				'per_page' => intval( $args['limit'] ),
-				'columns'  => intval( $args['columns'] ),
-			) );
-
-			do_action( 'storefront_homepage_after_popular_products' );
-
-			echo '</section>';
-		}
-	}
+function best_selling_products_title( $args ) {
+	$args['title'] = 'Los más vendidos'; // title ~> Título de la sección
+	return $args;
 }
+add_filter( 'storefront_best_selling_products_args', 'best_selling_products_title' );
+
+function popular_products_title( $args ) {
+	$args['title'] = 'Los más populares'; // title ~> Título de la sección
+	return $args;
+}
+add_filter( 'storefront_popular_products_args', 'popular_products_title' );
