1baaed0830e4c3e7662a78dd16ff8807446767eb
Alex Ballera <alexballera@gmail.com>
2016-07-31 09:01:07 -0400

    Mejorado menu dropdown

--- src/js/lib/menuDropdown.js
+++ src/js/lib/menuDropdown.js
@@ -1,11 +1,11 @@
 jQuery(function (jQuery) {
   var menuDropdown = jQuery('button#menuDropdown')
   var menuDropdownUl = jQuery('ul#menuDropdownUl')
-  menuDropdown.click(function () {
+  menuDropdown.hover(function () {
     menuDropdown.find('i#caretDown').toggleClass('hide')
     menuDropdown.find('i#caretUp').toggleClass('show-inline')
-    menuDropdownUl.toggleClass('show-block')
-    menuDropdownUl.toggleClass('hide')
+    menuDropdownUl.toggleClass('show-block').css('transition', 'all 1.2s ease')
+    menuDropdownUl.toggleClass('hide').css('transition', 'all 1.2s ease')
   })
   menuDropdownUl.mouseleave(function () {
     menuDropdownUl.toggleClass('show-block')
