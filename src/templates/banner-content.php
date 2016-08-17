db9ad51305cba46907d73c5db89261a2d2e6fc25
Alex Ballera <alexballera@gmail.com>
2016-08-17 13:16:53 -0400

    Agregado url en los banners de content

--- src/templates/banner-content.php
+++ src/templates/banner-content.php
@@ -1,11 +1,17 @@
 <div class="content-area__slides">
   <picture>
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content-1.png" />
+    <a href="<?php home_url();?>/producto-categoria/pantalones/">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content-1.jpeg" />
+    </a>
   </picture>
   <picture>
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content-2.png" />
+    <a href="<?php home_url();?>/producto-categoria/blusa/">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content-2.jpeg" />
+    </a>
   </picture>
   <picture>
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content-3.png" />
+    <a href="<?php home_url();?>/producto-categoria/pantalones/">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content-3.jpeg" />
+    </a>
   </picture>
 </div>
