a0810431af74fe7d4410df6247f61b3d8c9daeca
Alex Ballera <alexballera@gmail.com>
2016-08-19 15:08:32 -0400

    Eliminado breadcrumb

--- src/inc/custom-template-hook.php
+++ src/inc/custom-template-hook.php
@@ -8,6 +8,7 @@ function custom_storefront_template_hook() {
 	remove_action( 'storefront_header', 'storefront_header_cart', 60);
 	remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
 	remove_action( 'storefront_footer', 'storefront_credit', 20);
+	remove_action( 'storefront_content_top',             'woocommerce_breadcrumb',                 10 );
 	add_action( 'storefront_header', 'storefront_secondary_navigation_wrapper', 22 );
 	add_action( 'storefront_header', 'banner_header', 25 );
 	add_action( 'storefront_header', 'header_cart', 35 );
