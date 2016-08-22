091bffb01ccf3ebdba2b535dc9573313cf0987d3
Alex Ballera <alexballera@gmail.com>
2016-08-22 07:26:40 -0400

    Eliminado google ads script

--- src/templates/browsehappy.php
+++ src/templates/browsehappy.php
@@ -5,10 +5,3 @@
     Para  mejorar la  experiencia..
   </p>
 <![endif]-->
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-1689752734604820",
-    enable_page_level_ads: true
-  });
-</script>
