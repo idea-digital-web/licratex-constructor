72b6fd74571d943f9c6ac2eda9e0548505004233
Alex Ballera <alexballera@gmail.com>
2016-08-22 10:33:39 -0400

    Cambiado banner principal x error ortográfico

--- src/templates/banner-principal.php
+++ src/templates/banner-principal.php
@@ -3,7 +3,7 @@
 		<ul class="slides">
 			<li>
 				<a href="<?php home_url();?>/producto-categoria/pijama/">
-					<img src="http://i1.wp.com/tiendas.ideapruebas.com/wp-content/uploads/2016/08/banner-principal-1.jpg">
+					<img src="http://i2.wp.com/tiendas.ideapruebas.com/wp-content/uploads/2016/08/banner-principal-1.jpeg">
 				</a>
 			</li>
 			<li>
