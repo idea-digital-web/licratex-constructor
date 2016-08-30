432d6fe14881c4edb383b75bee0911c802e7556b
Alex Ballera <alexballera@gmail.com>
2016-08-30 08:25:13 -0400

    Quitado form de localhost

--- src/templates/footer-content.php
+++ src/templates/footer-content.php
@@ -33,7 +33,7 @@
 	<!-- IdeaDigital -->
 	<?php if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='5'][/fc]"); } ?>
 	<!-- Localhost -->
-	<?php if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='1'][/fc]"); } ?>
+	<!-- <?php if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='1'][/fc]"); } ?> -->
 </div>
 <div class="site-footer__item">
 	<h2>
