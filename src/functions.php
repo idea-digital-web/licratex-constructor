3897dd5047b3c5b30637bbdf37d46f01d6ee78e7
Alex Ballera <alexballera@gmail.com>
2016-09-08 14:08:28 -0400

    Agregada funcionalidad de localhost

--- src/functions.php
+++ src/functions.php
@@ -37,7 +37,7 @@ require 'inc/minifier-script.php';
 require 'inc/custom-field-billing.php';
 
 // Add alt tags to img
-require 'inc/add-alt-tags.php'
+require 'inc/add-alt-tags.php';
 
 // Dev localhost; comentar en modo producción
 require 'inc/localhost.php';
